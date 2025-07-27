437ca752c58c55b70ad233b93c0b9a205129b113
Sarthak_Gupta <sarthak75220@gmail.com>
2025-07-28 01:25:44 +0530

    Refactor routing in App component: add Root component for user redirection based on authentication status

--- Frontend/expense-tracker/src/App.jsx
+++ Frontend/expense-tracker/src/App.jsx
@@ -1,20 +1,23 @@
 import React from 'react'
-import { Route, Router, Routes } from 'react-router-dom'
+import { Navigate, Route, Router, Routes } from 'react-router-dom'
 import Home from "./Pages/Dashboard/Home.jsx"
 import Login from "./Pages/Auth/Login.jsx"
 import SignUp from "./Pages/Auth/SignUp.jsx"
 import Expense from "./Pages/Dashboard/Expense.jsx";
 import Income from "./Pages/Dashboard/Income.jsx";
 import toast, { Toaster } from 'react-hot-toast';
+import { useContext } from 'react'
+import { userDataContext } from './Context/userContext.jsx'
 
 const App = () => {
   return (
     <div>
       <Toaster />
         <Routes>
-          <Route path='/' element={<Home />} />
+          <Route path='/' element={<Root />} />
           <Route path='/login' element={<Login />} />
           <Route path='/signup' element={<SignUp />} />
+          <Route path='/dashboard' element={<Home />} />
           <Route path='/expense' element={<Expense />} />
           <Route path='/income' element={<Income />} />     
         </Routes>
@@ -23,4 +26,14 @@ const App = () => {
   )
 }
 
-export default App
+const Root = () => {
+  const { userData } = useContext(userDataContext);
+
+  return userData ? (
+    <Navigate to="/dashboard" />
+  ) : (
+    <Navigate to="/login" />
+  )
+}
+
+export default App;
