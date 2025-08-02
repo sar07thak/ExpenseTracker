50ab9c62ad6f75e7e87dcbfc3393d2f2821ed3f2
Sarthak_Gupta <sarthak75220@gmail.com>
2025-08-02 10:55:52 +0530

    fix: update import path for UserContext in main.jsx

--- Frontend/expense-tracker/src/main.jsx
+++ Frontend/expense-tracker/src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import ServerContext from './Context/ServerContext.jsx';
-import UserContext from './Context/userContext.jsx';
+import  UserContext  from '../Context/UserContext';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
